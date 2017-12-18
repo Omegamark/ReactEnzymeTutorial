6bb91a5280355e0eda5c867e458b6f383bbf4280
Mark Grant <grant.mark@gmail.com>
2017-12-18 10:03:33 -0700

    working on testing stack component

--- 02_notetoself/flashcardpro/src/components/Stack.test.js
+++ 02_notetoself/flashcardpro/src/components/Stack.test.js
@@ -1,7 +1,39 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Stack from './Stack'
+
+/* The Stack component is in curlies here since Stack.js itself is a connected component
+which we cannot test (at least easily) by itself. So I have exported the class Stack (see Stack.js)
+instead of the exported, connected component. This allows me to test the class rather than the
+exported component */
+import { Stack } from './Stack'
+import { stack } from '../data/fixtures'
+
+// The Stack component which has been imported needs a notion of props.
+const props = { stack }
+
+// This is now a data object called fixtures.js within the ./data folder
+// const props = {
+//     stack: {
+//         id: 0,
+//         title: 'test title',
+//         cards: [{ id: 0, prompt: 'test prompt', answer: 'test answer' },
+//                 { id: 1, prompt: 'test prompt deux', answer: 'test answwer deux'}]
+//     }
+// }
 
 describe('Stack', () => {
-    const stack = shallow(<Stack />)
+    const stack = shallow(<Stack {...props} />)
+
+    it('renders the title', () => {
+        // console.log(stack.debug())
+        expect(stack.find('h3').text()).toEqual(props.stack.title)
+    })
+
+    it('renders the Link home', () => {
+        expect(stack.find('Link h4').text()).toEqual('Home')
+    })
+
+    it('renders the correct number of cards', () => {
+        expect(stack.find('Card').length).toEqual(props.stack.cards.length)
+    })
 })
